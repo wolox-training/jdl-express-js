24bea2a964392f48f421df15ab3b5c6b88a2bd5b
Juan David Alvarez <juan.alvarez@wolox.co>
2018-10-25 15:19:09 -0500

    adapting to  the standard naming

--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
+  const User = sequelize.define('users', {
     name: {
       allowNull: false,
       type: DataTypes.STRING
