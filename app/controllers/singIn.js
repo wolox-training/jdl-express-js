277bfb17dc52c0df7d5a45bafb00e278e175f1ba
Juan David Alvarez <juan.alvarez@wolox.co>
2018-10-29 09:01:36 -0500

    finishing refactor

--- app/controllers/singIn.js
+++ app/controllers/singIn.js
@@ -1,10 +1,10 @@
-function validEmail(email) {
+const validEmail = email => {
   const emailRexEx = new RegExp('^w+@wolox+?.[a-zA-Z]{2,3}$');
   return !!emailRexEx.test(email);
-}
-function validPassword(password) {
+};
+const validPassword = password => {
   return password.lengh() === 8;
-}
-function hasErrors(user) {
+};
+const hasErrors = user => {
   return !(validEmail(user.email) && validPassword(user.password));
-}
+};
