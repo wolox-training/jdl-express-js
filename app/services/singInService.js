efb81055292cc5224951b947365882fec347211c
Juan David Alvarez <juan.alvarez@wolox.co>
2018-10-30 11:13:20 -0500

    retiring un used imports.

--- app/services/singInService.js
+++ app/services/singInService.js
@@ -1,15 +1,1 @@
-const express = require('express');
-
-let singin;
-const routerserv = express.Router;
-
-// posting into users
-routerserv.post('/user', function(user) {
-  if (!singin.haserrors(user)) {
-    // post on users table after encrypting the pw
-    // log message
-  } else {
-    // log error message
-  }
-});
 // # note: nothing really works here
