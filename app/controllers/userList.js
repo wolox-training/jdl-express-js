4b2291c0b384261ffebf3c764d1128cd4e291e77
Juan David Alvarez <juan.alvarez@wolox.co>
2018-11-01 10:50:30 -0500

    returning the list of users in the service

--- app/controllers/userList.js
+++ app/controllers/userList.js
@@ -3,7 +3,7 @@ const _user = require('./../models').user;
 exports.userList = (req, res) => {
   const usermail = req.body.email;
   if (_user.findAll({ attributes: ['sesion'] }, { where: { email: usermail } })) {
-    _user
+    return _user
       .findAll({ attributes: ['name', 'lastName', 'email', 'role'] }, { limit: 5 })
       .then(userlist => {
         res.json(userlist).end();
