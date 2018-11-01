be3210ae2a7f644064dad73deac9e9110b0ef928
Juan David Alvarez <juan.alvarez@wolox.co>
2018-11-01 08:42:54 -0500

    adding session validation

--- app/controllers/userList.js
+++ app/controllers/userList.js
@@ -2,13 +2,15 @@ const _user = require('./../models').user;
 
 exports.userList = (req, res) => {
   const usermail = req.body.email;
-  // if (_user.findAll({ attributes: ['sesion'] }, { where: { email: usermail } })) {
-  _user
-    .findAll({ attributes: ['name', 'lastName', 'email', 'role'] }, { limit: 5 })
-    .then(userlist => {
-      res.json(userlist).end();
-    })
-    .catch(req.error)
-    .throw(req.error);
-  //  }
+  if (_user.findAll({ attributes: ['sesion'] }, { where: { email: usermail } })) {
+    _user
+      .findAll({ attributes: ['name', 'lastName', 'email', 'role'] }, { limit: 5 })
+      .then(userlist => {
+        res.json(userlist).end();
+      })
+      .catch(err => {
+        res.status(503);
+        res.send(err);
+      });
+  } else res.status(401).send('Error, you need to sign In before performing this action');
 };
