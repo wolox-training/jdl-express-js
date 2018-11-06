41ba9345bfffc1d27b3d0ee498c205a1e52d672b
Juan David Alvarez <juan.alvarez@wolox.co>
2018-11-06 12:29:42 -0500

    refactoring adminUser function so it works correctly with promises

--- app/controllers/singIn.js
+++ app/controllers/singIn.js
@@ -17,21 +17,41 @@ const hashPassword = password => {
   return crypt.hash(password, 10);
 };
 
-exports.admUser = async (req, res) => {
+exports.admUser = (req, res) => {
   const userToUpdate = req.body.updUser;
   const userUpdater = req.body.userUpdater;
-  if (usController.activeSesion(userUpdater) && usController.isAdmin(userUpdater)) {
-    const user = await _user.findAll({ where: { email: userToUpdate.email } });
-    user.update({ role: ' admin ' });
-    user.save().then(res.send(`the user ${userToUpdate} now has the role Admin`).status(200));
-  } else {
-    this.signUp(userToUpdate)
-      .then(created => {
-        res.json(created).end();
-      })
-      .catch(req.error)
-      .throw(req.error);
-  }
+  usController.exist(userToUpdate).then(exist => {
+    if (!exist) {
+      exports
+        .signUp(userToUpdate)
+        .then(created => {
+          res.json(created).end();
+        })
+        .catch(req.error)
+        .throw(req.error);
+    } else {
+      usController.authenticated(userUpdater).then(userauth => {
+        if (userauth) {
+          usController.isAdmin(userUpdater).then(admin => {
+            if (admin) {
+              return _user
+                .findAll({ where: { email: userToUpdate.email } })
+                .then(user => {
+                  user.update({ role: ' admin ' });
+                })
+                .then(res.send(`the user ${userToUpdate} now has the role Admin`).status(200));
+            } else {
+              res
+                .send(
+                  `Error. credential no found, please make sure that ${userUpdater}, has an active session and is an admin`
+                )
+                .status(401);
+            }
+          });
+        }
+      });
+    }
+  });
 };
 
 exports.signUp = async (req, res) => {
--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,21 +3,21 @@ const _user = require('./../models').user,
   secretk = require('../../config/index').config;
 
 const exist = usermail => {
-  return _user.findWhere({ email: usermail }).then(user => {
-    user !== null || user !== undefined;
+  return _user.findAll({ where: { email: usermail } }).then(user => {
+    return !!user;
   });
 };
 
-const isAdmin = async user => {
+const isAdmin = user => {
   const usermail = user.email;
-  return _user.findWhereParam(['role'], { email: usermail }).then(role => {
-    role === 'admin';
+  return _user.findAll({ params: ['role'] }, { where: { email: usermail } }).then(role => {
+    return role === 'admin';
   });
 };
 
 const authenticated = req => {
   const token = req.cookie;
   return jwt.verify(token, secretk.session.secret).then(decoded => {
-    exist(decoded);
+    return exist(decoded);
   });
 };
