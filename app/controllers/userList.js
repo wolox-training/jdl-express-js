9f05b7ce11a9709b21ad779b16062a2d6ecc64f3
Juan David Alvarez <juan.alvarez@wolox.co>
2018-11-06 16:58:03 -0500

    adding returns

--- app/controllers/userList.js
+++ app/controllers/userList.js
@@ -2,7 +2,7 @@ const _user = require('./../models').user,
   userControl = require('./../controllers/userController');
 
 exports.userList = async (req, res) => {
-  userControl.authenticated(req).then(authenticated => {
+  return userControl.authenticated(req).then(authenticated => {
     if (authenticated) {
       const limit = req.body.limit;
       const pages = req.body.page;
@@ -14,12 +14,12 @@ exports.userList = async (req, res) => {
         .then(userlist => {
           const numberOfUsers = userlist.count;
           const page = Math.ceil(numberOfUsers / limit);
-          res.json(userlist, { numberOfUsers }, { numberOfPages: page }).end();
+          return res.json(userlist, { numberOfUsers }, { numberOfPages: page }).end();
         })
         .catch(err => {
           res.status(503);
           res.send(err);
         });
-    } else res.status(401).send('Error, you need to sign In before performing this action');
+    } else return res.status(401).send('Error, you need to sign In before performing this action');
   });
 };
