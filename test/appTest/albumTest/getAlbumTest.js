48adf05b2b02df0a6b001f350f2eb892d5786710
Juan David Alvarez <juan.alvarez@wolox.co>
2018-11-16 13:08:54 -0500

    CI test

--- test/appTest/albumTest/getAlbumTest.js
+++ test/appTest/albumTest/getAlbumTest.js
@@ -4,7 +4,7 @@ const url = process.env.API_URL,
 
 describe('Getting the album list from a provided API ', () => {
   it('if the User is loged in with a correct token a list of albums will be recived in JSON format', done => {
-    nock(url, {
+    nock(`'${url}'`, {
       reqheaders: {
         asccesToken: validToken
       }
@@ -17,7 +17,7 @@ describe('Getting the album list from a provided API ', () => {
 
 describe('Getting the album list from a provided API, but not loged In ', () => {
   it('if the User is not loged in with a correct token the request wont succed and a message will apear', done => {
-    nock(url)
+    nock(`'${url}'`)
       .get('/albums')
       .reply(401, 'unauthorized acces, you must log In before performing this action')
       .then(done());
